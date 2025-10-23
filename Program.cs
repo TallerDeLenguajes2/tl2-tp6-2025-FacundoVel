723a7528f0d462803237cc3a50d51f39cc8df157
FacundoVel <facundo.vel99@gmail.com>
2025-10-23 20:48:46 -0300

    Se archivo para implementar los ejemplos del TP

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
-string connectionString = "Data Source=base_test.db;";
+string connectionString = "Data Source=Tienda.db;";
 
 // Crear conexión a la base de datos
 using (SqliteConnection connection = new SqliteConnection(connectionString))
@@ -14,25 +14,88 @@ using (SqliteConnection connection = new SqliteConnection(connectionString))
         createTableCmd.ExecuteNonQuery();
         Console.WriteLine("Tabla 'productos' creada o ya existe.");
     }
-    
-    // Insertar datos
-    string insertQuery = "INSERT INTO productos (nombre, precio) VALUES ('Manzana', 0.50), ('Banana', 0.30)";
-            using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
-            {
-                insertCmd.ExecuteNonQuery();
-                Console.WriteLine("Datos insertados en la tabla 'productos'.");
-            }
-    // Leer datos
-            string selectQuery = "SELECT * FROM productos";
-            using (SqliteCommand selectCmd = new SqliteCommand(selectQuery, connection))
-            using (SqliteDataReader reader = selectCmd.ExecuteReader())
-            {
-                Console.WriteLine("Datos en la tabla 'productos':");
-                while (reader.Read())
-                {
-                    Console.WriteLine($"ID: {reader["id"]}, Nombre: {reader["nombre"]}, Precio: {reader["precio"]}");
-                }
-            }
 
-            connection.Close();
+    //insertar un producto
+    string insertProducto = "INSERT INTO Productos (Descripcion, Precio) VALUES ('Mouse Inalambrico Logitech', 5000.0)";
+    using (SqliteCommand cmd = new SqliteCommand(insertProducto, connection))
+    {
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Producto insertado correctamente.");
+    }
+
+    //Insertar un presupuesto
+    string insertPresupuesto = "INSERT INTO Presupuestos (NombreDestinatario, FechaCreacion) VALUES ('Carlos Ruiz', '2024-10-25')";
+    using (SqliteCommand cmd = new SqliteCommand(insertPresupuesto, connection))
+    {
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Presupuesto insertado correctamente.");
+    }
+
+    //insertar registros en PresupuestoDetalle
+    string insertDetalle = "INSERT OR IGNORE INTO PresupuestosDetalle (idPresupuesto, idProducto, Cantidad) VALUES (1, 3, 2)";
+    using (SqliteCommand cmd = new SqliteCommand(insertDetalle, connection))
+    {
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Detalle insertado correctamente.");
+    }
+
+    //Modificar un Producto
+    string updateProducto = "UPDATE Productos SET Descripcion = 'Teclado Mecanico Logitech', Precio = 12000 WHERE idProducto = 3";
+    using (SqliteCommand cmd = new SqliteCommand(updateProducto, connection))
+    {
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Se modifico un producto Correctamente");
+    }
+
+    //Modificar NombreDestinatario de Presupuesto
+    String updatePresupuesto = "UPDATE Presupuestos SET NombreDestinatario = 'Luis Fernandez' WHERE idPresupuesto = 1";
+    using (SqliteCommand cmd = new SqliteCommand(updatePresupuesto, connection))
+    {
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Presupuesto actualizado");
+    }
+
+    //Eliminar un registro de PresupuestoDetalle
+    string deleteDetalle = "DELETE FROM PresupuestosDetalle WHERE idPresupuesto = 1 AND idProducto = 2";
+    using (SqliteCommand cmd = new SqliteCommand(deleteDetalle, connection))
+    {
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Registro Eliminado");
+    }
+
+    Console.WriteLine("\nTABLA: Productos");
+    string selectProductos = "SELECT * FROM Productos";
+    using (SqliteCommand cmd = new SqliteCommand(selectProductos, connection))
+    using (SqliteDataReader reader = cmd.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            Console.WriteLine($"ID: {reader["idProducto"]} | Descripcion: {reader["Descripcion"]} | Precio: {reader["Precio"]}");
+        }
+    }
+
+    Console.WriteLine("\nTABLA: Presupuestos");
+    string selectPesupuestos = "SELECT * FROM Presupuestos";
+    using (SqliteCommand cmd = new SqliteCommand(selectPesupuestos, connection))
+    using (SqliteDataReader reader = cmd.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            Console.WriteLine($"ID: {reader["idPresupuesto"]} | Nombre: {reader["NombreDestinatario"]} | Fecha: {reader["FechaCreacion"]}");
+        }
+    }
+
+    Console.WriteLine("\nTABLA: PresupuestosDetalle");
+    string selectDetalles = "SELECT * FROM PresupuestosDetalle";
+    using (SqliteCommand cmd = new SqliteCommand(selectDetalles, connection))
+    using (SqliteDataReader reader = cmd.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            Console.WriteLine($"idPresupuesto: {reader["idPresupuesto"]} | idProducto: {reader["idProducto"]} | Cantidad: {reader["Cantidad"]}");
+        }
+    }
+
+    connection.Close();
+    Console.WriteLine("desconexion de la db :)");
 }
